2aab5aab9ef96cfe0c340daf154498f603dad5b5
goofrnzik <goofrnzik@gmail.com>
2020-12-22 18:27:47 +0200

    set the mat toolbar

--- globalTasks/src/app/app.module.ts
+++ globalTasks/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,6 +15,8 @@ import { MatSliderModule } from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],
